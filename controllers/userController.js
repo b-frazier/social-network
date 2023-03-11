027f6e8bd729064f486955babcf9b95569743945
bfrazier <bthehippie2@gmail.com>
2023-03-11 17:54:13 -0500

    add populate for thoughts & friends; add catch

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,10 +12,12 @@ module.exports = {
   getOneUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .select('-__v')
+      .populate('thoughts', 'friends')
       .then((user) =>
         user
           ? res.json(user)
           : res.status(404).json({ message: 'No user found with that ID' })
-      );
+      )
+      .catch((err) => res.status(500).json(err));
   },
 };
