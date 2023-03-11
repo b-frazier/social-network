bde9735a91ca3c9f937386c8fc4ddfdc6fe8e058
bfrazier <bthehippie2@gmail.com>
2023-03-11 17:38:05 -0500

    functions for finding all & one user

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const { ObjectId } = require('mongoose').Types;
+const { User } = require('../models');
+
+module.exports = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // get one user
+  getOneUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) =>
+        user
+          ? res.json(user)
+          : res.status(404).json({ message: 'No user found with that ID' })
+      );
+  },
+};
