5a9044942ca8cbc72b4c03932cea20eeaadfa2f4
bfrazier <bthehippie2@gmail.com>
2023-03-11 11:35:42 -0500

    update port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 // initialize express
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URL);
