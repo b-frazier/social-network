d3dfac036d72b017d60e02f089e31504b74b1a31
bfrazier <bthehippie2@gmail.com>
2023-03-11 11:55:10 -0500

    write server start and uses

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 // install packages
 const express = require('express');
-
 require('dotenv').config();
+
+// routes
+const db = require('./config/connection');
+const routes = require('./routes');
 
 // initialize express
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Running on port ${PORT} and connected to mongoose`);
+  });
+});
