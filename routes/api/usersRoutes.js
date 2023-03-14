b37888b95557350f1d804b2e167ac76431b3e413
bfrazier <bthehippie2@gmail.com>
2023-03-13 22:29:40 -0400

    include addFriend function; write route

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -5,7 +5,9 @@ const {
   postUser,
   updateUser,
   deleteUser,
+  addFriend,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(postUser);
 router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete();
