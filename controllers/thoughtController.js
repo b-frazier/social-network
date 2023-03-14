a82360ae48aeb8e92d1e6e1db5d694c14d60b484
bfrazier <bthehippie2@gmail.com>
2023-03-14 19:28:39 -0400

    write function to get all thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,11 @@
 const router = require('express').Router();
+const Thought = require('../models');
+
+module.exports = {
+  // get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+};
