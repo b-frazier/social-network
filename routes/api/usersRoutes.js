335ff97333eac518e4e90e276589437d6a3e7885
bfrazier <bthehippie2@gmail.com>
2023-03-13 22:19:48 -0400

    routes for user id

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -8,3 +8,4 @@ const {
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(postUser);
+router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser);
