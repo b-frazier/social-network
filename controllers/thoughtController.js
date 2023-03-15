3bd428d4e006940fe0ef9d60c4248076395c3719
bfrazier <bthehippie2@gmail.com>
2023-03-14 21:11:21 -0400

    write function to get one thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,4 +8,14 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
+  // get thoughts by id
+  getOneThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughId })
+      .then((thought) =>
+        thought
+          ? res.json(thought)
+          : res.status(404).json({ message: 'No thoughts found' })
+      )
+      .catch((err) => res.json(500).json(err));
+  },
 };
