b63605a45669309d26c1f55df91371074171ef94
bfrazier <bthehippie2@gmail.com>
2023-03-14 21:47:04 -0400

    add function to delete reaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   // get thoughts by id
   getOneThought(req, res) {
-    Thought.findOne({ _id: req.params.thoughId })
+    Thought.findOne({ _id: req.params.thoughtId })
       .then((thought) =>
         thought
           ? res.json(thought)
@@ -56,6 +56,34 @@ module.exports = {
               { $pull: { thoughts: req.params.thoughtId } },
               { new: true }
             )
+          : res.status(404).json({ message: 'No thought found' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // add a reaction
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        thought
+          ? res.json(thought)
+          : res.status(404).json({ message: 'No thought found' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // delete reaction
+  deleteReaction(req, res) {
+    Thought.findOneAndRemove(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionsId: req.params.reactionsId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        thought
+          ? res.json(thought)
           : res.status(404).json({ message: 'No thought found' })
       )
       .catch((err) => res.status(500).json(err));
