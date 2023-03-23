2587ee60161088edc31b57529a8726258fd019e9
bfrazier <bthehippie2@gmail.com>
2023-03-23 09:56:39 -0400

    update routes to add/delete reactions

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -15,7 +15,8 @@ router
   .route('/:thoughtId')
   .get(getOneThought)
   .put(updateThought)
-  .delete(deleteThought);
-router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
+  .delete(deleteThought)
+  .post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
