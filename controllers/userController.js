0040606ee580a5807652c2657011b5a17f64d589
bfrazier <bthehippie2@gmail.com>
2023-03-22 22:46:38 -0400

    separate the populate; change to findbyid

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,15 +10,19 @@ module.exports = {
   },
   // get one user
   getOneUser(req, res) {
-    User.findOne({ _id: req.params.userId })
+    console.log(req.params.userId);
+    User.findById({ _id: req.params.userId })
       .select('-__v')
-      .populate('thoughts', 'friends')
+      .populate('thoughts')
+      .populate('friends')
       .then((user) =>
         user
           ? res.json(user)
           : res.status(404).json({ message: 'No user found with that ID' })
       )
-      .catch((err) => res.status(500).json(err));
+      .catch((err) => {
+        res.status(500).json(err);
+      });
   },
   // post a user
   postUser(req, res) {
