bb1a496216cc7f56e484445f984f59fa0b5dd247
bfrazier <bthehippie2@gmail.com>
2023-03-22 23:06:55 -0400

    match naming

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -11,6 +11,9 @@ const {
 
 router.route('/').get(getUsers).post(postUser);
 router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser);
-router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+router
+  .route('/:userId/friends/:friendsId')
+  .post(addFriend)
+  .delete(deleteFriend);
 
 module.exports = router;
