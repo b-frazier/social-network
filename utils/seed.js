4465b33e370dd8eb63788528721d3a27a0d2520d
bfrazier <bthehippie2@gmail.com>
2023-03-24 22:49:26 -0400

    write loops to push to arrays & seed db

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,6 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models/index');
+const { finalEmail, usernames, thoughts } = require('./data');
 
 connection.on('error', (err) => err);
 connection.once('open', async () => {
@@ -8,11 +9,28 @@ connection.once('open', async () => {
   await User.deleteMany();
   await Thought.deleteMany();
 
+  const email = finalEmail;
+  const username = usernames;
+
   const users = [];
+  const finalThoughts = [];
 
-  // add thoughts & reactions
-
-  users.push({});
+  for (let i = 0; i < email.length; i++) {
+    users.push({
+      username: username[i],
+      email: email[i],
+      thoughtText: thoughts[i],
+    });
+    finalThoughts.push({
+      thoughtText: thoughts[i],
+      username: username[i],
+    });
+  }
 
   await User.collection.insertMany(users);
+  await Thought.collection.insertMany(finalThoughts);
+
+  console.log('_________________');
+  console.log('seeding is done');
+  process.exit(0);
 });
